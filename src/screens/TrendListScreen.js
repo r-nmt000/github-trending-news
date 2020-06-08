7f28b7fedc95b820e43ad3c060c8c8884e2d7b2b
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-06-09 00:40:23 +0900

    feat: load trending api

--- src/screens/TrendListScreen.js
+++ src/screens/TrendListScreen.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import {
+  languages,
+  spokenLanguages,
+  fetchRepositories,
+  fetchDevelopers,
+} from '@huchenme/github-trending';
 
 const TrendListScreen = () => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    fetchRepositories({language: 'ruby', since: 'monthly'}).then(
+      (repositories) => {
+        setRepos(repositories);
+      }
+    );
+  },[]);
+
+
   return (
     <View>
       <Text>list</Text>
