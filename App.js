8c0cb8d92ebf53682798d5fa3c3cc7cf9426888b
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-06-09 22:26:38 +0900

    feat: add empty drawer filter screen

--- App.js
+++ App.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import DrawerLayout from "react-native-drawer-layout";
 import TrendListScreen from "./src/screens/TrendListScreen";
 import TrendDetailScreen from "./src/screens/TrendDetailScreen";
+import SearchFilterScreen from "./src/screens/SearchFilterScreen";
+import {Button, Icon} from "react-native-elements";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const drawer = useRef(null);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="TrendList"
-      >
-        <Stack.Screen
-          name="TrendList"
-          component={TrendListScreen}
-        />
-        <Stack.Screen
-          name="TrendDetail"
-          component={TrendDetailScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <DrawerLayout
+      drawerBackgroundColor='white'
+      drawerPosition='right'
+      drawerWidth={250}
+      renderNavigationView={SearchFilterScreen}
+      ref={drawer}
+    >
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="TrendList"
+        >
+          <Stack.Screen
+            name="TrendList"
+            component={TrendListScreen}
+            options={{
+              headerRight: () => (
+                <Button
+                  type='clear'
+                  icon={
+                    <Icon
+                      name="filter"
+                      type="antdesign"
+                    />
+                  }
+                  onPress={()=>{
+                    drawer.current.openDrawer();
+                  }}
+                />
+              )
+            }}
+          />
+          <Stack.Screen
+            name="TrendDetail"
+            component={TrendDetailScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </DrawerLayout>
   )
 }
 
---
+++ src/screens/SearchFilterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchFilterScreen = () => {
+  return (
+    <View>
+      <Text>search filter screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchFilterScreen;
