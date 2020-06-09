a4c7dc0fbf9f642a48b6079e1fdd152b84b13673
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-06-09 20:52:33 +0900

    refactor: extract list item to component

---
+++ src/components/TrendListItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {Icon, ListItem} from "react-native-elements";
+
+const TrendListItem = ({name, avatar, description, stars, forks, author}) => {
+  return (
+    <ListItem
+      chevron
+      bottomDivider={true}
+      title={name}
+      titleStyle={styles.title}
+      leftAvatar={{source: {url: avatar}}}
+      subtitle={
+        <View>
+          <Text numberOfLines={1}> {description} </Text>
+          <View style={styles.iconContainer}>
+            <View style={styles.statsContainer}>
+              <Icon name='star-o' type='font-awesome' size='14' />
+              <Text> {stars}</Text>
+            </View>
+            <View style={styles.statsContainer}>
+              <Icon name='code-fork' type='font-awesome' size='14' />
+              <Text> {forks}</Text>
+            </View>
+            <View style={styles.statsContainer}>
+              <Icon name='person-outline' type='material' size='14' />
+              <Text> {author}</Text>
+            </View>
+          </View>
+        </View>
+      }
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#27f',
+    fontWeight: 'bold'
+  },
+  iconContainer: {
+    marginTop: 12,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  statsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  }
+});
+
+export default TrendListItem;
--- src/screens/TrendListScreen.js
+++ src/screens/TrendListScreen.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
-import {Icon, ListItem} from "react-native-elements";
+import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import {
   languages,
   spokenLanguages,
   fetchRepositories,
   fetchDevelopers,
 } from '@huchenme/github-trending';
+import TrendListItem from "../components/TrendListItem";
 
 const TrendListScreen = ({navigation}) => {
   const [repos, setRepos] = useState(null);
@@ -35,31 +35,13 @@ const TrendListScreen = ({navigation}) => {
               onPress={() => {
               navigation.navigate('TrendDetail', {url: item.url});
             }}>
-              <ListItem
-                chevron
-                bottomDivider={true}
-                title={item.name}
-                titleStyle={styles.title}
-                leftAvatar={{source: {url: item.avatar}}}
-                subtitle={
-                  <View>
-                    <Text numberOfLines={1}> {item.description} </Text>
-                    <View style={styles.iconContainer}>
-                      <View style={styles.statsContainer}>
-                        <Icon name='star-o' type='font-awesome' size='14' />
-                        <Text> {item.stars}</Text>
-                      </View>
-                      <View style={styles.statsContainer}>
-                        <Icon name='code-fork' type='font-awesome' size='14' />
-                        <Text> {item.forks}</Text>
-                      </View>
-                      <View style={styles.statsContainer}>
-                        <Icon name='person-outline' type='material' size='14' />
-                        <Text> {item.author}</Text>
-                      </View>
-                    </View>
-                  </View>
-                }
+              <TrendListItem
+                name={item.name}
+                avatar={item.avatar}
+                description={item.description}
+                stars={item.stars}
+                forks={item.forks}
+                author={item.author}
               />
             </TouchableOpacity>
           )
@@ -71,21 +53,6 @@ const TrendListScreen = ({navigation}) => {
 };
 
 const styles = StyleSheet.create({
-  title: {
-    color: '#27f',
-    fontWeight: 'bold'
-  },
-  iconContainer: {
-    marginTop: 12,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  statsContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-  }
-
 });
 
 export default TrendListScreen;
