9b9be556000b87935b92c8d5b4784db3620de3fe
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-06-16 00:19:03 +0900

    feat: add loading indicator to detail screen

--- src/screens/TrendDetailScreen.js
+++ src/screens/TrendDetailScreen.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { WebView } from "react-native-webview";
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
 
 const TrendDetailScreen = ({route}) => {
   const url = route.params?.url || null;
+  const [isLoading, setLoading] = useState(true);
   return (
-    <WebView source={{ uri: url}}/>
+    <View style={styles.container}>
+      <WebView
+        onLoadStart={() => {setLoading(true)}}
+        onLoad={() => {setLoading(false)}}
+        source={{ uri: url}}/>
+      {isLoading && (
+        <ActivityIndicator size="large" style={styles.indicator}/>
+      )}
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  indicator: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+  }
+});
 
 export default TrendDetailScreen;
